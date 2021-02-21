0f9baed974c44285654f2a23e930b8a9ce8799f1
999RRub <293653@supinfo.com>
2021-02-21 17:58:44 +0100

    changement couleur pour les yeux

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { ViewEncapsulation } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { ProduitDetailComponent } from './graphics/produit-detail/produit-detail.component';
 import { InterfaceService } from './services/interface.service';
 import { ProduitServiceService } from './services/produit-service.service';
@@ -6,11 +7,13 @@ import { ProduitServiceService } from './services/produit-service.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
   title = 'AppTennisPlayer';
 
   
   public constructor(public interfaceService: InterfaceService, public produitService: ProduitServiceService){}
+  
 }
