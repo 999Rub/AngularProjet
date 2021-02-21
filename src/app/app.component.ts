bbae57dd78f4472a6a3dc4a799f79520486f7fa4
999RRub <293653@supinfo.com>
2021-02-21 17:46:03 +0100

    garder le form viergre

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ProduitDetailComponent } from './graphics/produit-detail/produit-detail.component';
 import { InterfaceService } from './services/interface.service';
+import { ProduitServiceService } from './services/produit-service.service';
 
 @Component({
   selector: 'app-root',
@@ -11,5 +12,5 @@ export class AppComponent {
   title = 'AppTennisPlayer';
 
   
-  public constructor(public interfaceService: InterfaceService){}
+  public constructor(public interfaceService: InterfaceService, public produitService: ProduitServiceService){}
 }
--- src/app/services/produit-service.service.ts
+++ src/app/services/produit-service.service.ts
@@ -63,6 +63,10 @@ export class ProduitServiceService {
     
   }
 
+  clearForm(){
+    this.produitForm.reset();
+  }
+
   
 
   
